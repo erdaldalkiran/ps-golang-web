b0d10825963f36f549c6d5aee325a64b2dfd2986
Erdal Dalkiran <ed@erdaldalkiran.com>
2017-03-05 19:31:59 +0000

    #000 ed ch2

--- main.go
+++ main.go
@@ -1,13 +1,55 @@
 package main
 
 import (
-  "net/http"
+	"bufio"
+	"net/http"
+	"os"
+	"path"
 )
 
 func main() {
-  http.HandleFunc("/", func( resp http.ResponseWriter, req *http.Request){
-    resp.Write([]byte("erdal was here!"))
-  })
-  
-  http.ListenAndServe(":8080", nil)
+	http.Handle("/", new(myHandler))
+	http.ListenAndServe(":8080", nil)
+}
+
+type myHandler struct {
+	http.Handler
+}
+
+func (handler *myHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	filePath := "public" + req.URL.Path
+
+	contentType := getContentType(filePath)
+	resp.Header().Add("Content-Type", contentType)
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		resp.WriteHeader(404)
+		resp.Write([]byte("404 - " + err.Error()))
+		return
+	}
+
+	bufferedReader := bufio.NewReader(f)
+	bufferedReader.WriteTo(resp)
+}
+
+func getContentType(filePath string) string {
+	var contentType string
+	ext := path.Ext(filePath)[1:]
+	switch ext {
+	case "html":
+		contentType = "text/html"
+	case "css":
+		contentType = "text/css"
+	case "png":
+		contentType = "image/png"
+	case "js":
+		contentType = "application/javascript"
+	case "mp4":
+		contentType = "video/mp4"
+	default:
+		contentType = "text/plain"
+	}
+
+	return contentType
 }
