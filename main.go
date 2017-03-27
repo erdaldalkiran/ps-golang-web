268cb0a3a89a3a52c51c12cf8409b5983584384c
Erdal Dalkiran <ed@erdaldalkiran.com>
2017-03-27 01:27:42 +0000

    #000 ed controllers-1 creating controller package and moving controller related code from main to the package

--- main.go
+++ main.go
@@ -1,138 +1,20 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"html/template"
-	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 
-	"io/ioutil"
-
-	"github.com/erdalkiran/ps-golang-web/viewmodels"
-	"github.com/oxtoacart/bpool"
+	"github.com/erdalkiran/ps-golang-web/controllers"
 )
 
-var templates map[string]*template.Template
-var bufpool *bpool.BufferPool
-
 func main() {
-	initialize()
+	initilaize()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		path := req.URL.Path[1:]
-
-		err := renderTemplate(w, path, getContext(path))
-		if err != nil {
-			handleError(w, err)
-		}
-	})
-
-	http.HandleFunc("/img/", func(w http.ResponseWriter, req *http.Request) {
-		err := serverResource(w, "public"+req.URL.Path, "image/png")
-		if err != nil {
-			handleError(w, err)
-		}
-	})
-
-	http.HandleFunc("/css/", func(w http.ResponseWriter, req *http.Request) {
-		err := serverResource(w, "public"+req.URL.Path, "text/css")
-		if err != nil {
-			handleError(w, err)
-		}
-	})
+	controllers.Register()
 
 	http.ListenAndServe(":8080", nil)
 
 }
 
-func getContext(path string) interface{} {
-	var context interface{}
-
-	switch path {
-	case "home":
-		context = viewmodels.NewHome()
-	case "categories":
-		context = viewmodels.NewCategories()
-	case "products":
-		context = viewmodels.NewProducts()
-	case "product":
-		context = viewmodels.NewProduct()
-	}
-	return context
-}
-
-func handleError(w http.ResponseWriter, err error) {
-	w.WriteHeader(500)
-	w.Write([]byte(err.Error()))
-}
-
-func serverResource(w http.ResponseWriter, fileName string, contentType string) error { //use struct instead of seperate parameters
-	file, err := os.Open(fileName)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	reader := bufio.NewReader(file)
-	w.Header().Set("Content-Type", contentType)
-	reader.WriteTo(w)
-
-	return nil
-}
-
-func initialize() {
-	bufpool = bpool.NewBufferPool(32)
-
-	if templates == nil {
-		templates = make(map[string]*template.Template)
-	}
-
-	const templatesDir = "templates/" //move to config
-
-	commonTemplates, err := filepath.Glob(templatesDir + "common/*.tmpl")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pagesDir := templatesDir + "pages/"
-	pageDirs, err := ioutil.ReadDir(pagesDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, pageDir := range pageDirs {
-		pageDirPath := filepath.Join(pagesDir, pageDir.Name())
-
-		pageTemplates, err := filepath.Glob(pageDirPath + "/*.tmpl")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		files := append(commonTemplates, pageTemplates...)
-		pageName := filepath.Base(pageDir.Name())
-
-		templates[pageName] = template.Must(template.ParseFiles(files...))
-	}
-
-}
-
-func renderTemplate(w http.ResponseWriter, name string, data interface{}) error {
-	tmpl, ok := templates[name]
-	if !ok {
-		return fmt.Errorf("the template %s does not exist", name)
-	}
-	buff := bufpool.Get()
-	defer bufpool.Put(buff)
-
-	err := tmpl.ExecuteTemplate(buff, "base.tmpl", data)
-	if err != nil {
-		return err
-	}
-
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	buff.WriteTo(w)
-	return nil
+func initilaize() {
+	controllers.Initialize()
 }
